58761e84649a11f02cdbc696c153f7f9d28343c9
Samyc2002 <2000200402@iitdh.ac.in>
2022-10-13 14:06:10 +0530

    Added Lab 1

---
+++ Lab1/Hello_World/Release/main.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdio.h
+main.obj: /opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: /opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h
+
+../main.c: 
+/opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdio.h: 
+/opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/linkage.h: 
+/opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h: 
---
+++ Lab1/xLED/Debug/led_test.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+led_test.obj: ../led_test.c
+led_test.obj: /opt/ti/ccsv6/ccs_base/emulation/boards/usbstk5515_v1/include/usbstk5515.h
+led_test.obj: /opt/ti/ccsv6/ccs_base/emulation/boards/usbstk5515_v1/include/usbstk5515_led.h
+led_test.obj: /opt/ti/ccsv6/ccs_base/emulation/boards/usbstk5515_v1/include/usbstk5515.h
+led_test.obj: /opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdio.h
+led_test.obj: /opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/linkage.h
+led_test.obj: /opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h
+
+../led_test.c: 
+/opt/ti/ccsv6/ccs_base/emulation/boards/usbstk5515_v1/include/usbstk5515.h: 
+/opt/ti/ccsv6/ccs_base/emulation/boards/usbstk5515_v1/include/usbstk5515_led.h: 
+/opt/ti/ccsv6/ccs_base/emulation/boards/usbstk5515_v1/include/usbstk5515.h: 
+/opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdio.h: 
+/opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/linkage.h: 
+/opt/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h: 
